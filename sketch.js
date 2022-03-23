03e334954208031583b208d55117f89ae3a6cc14
AnaLopes99 <102222313+AnaLopes99@users.noreply.github.com>
2022-03-23 12:56:25 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,9 +8,32 @@ function setup() {
 
 function draw() {
 
+background(0)
+
+
+if(mouseY >= height/2) {
+noFill();
+}
+
+else{
+
+fill(12,108,57); //verde
+
+
+
+}
+
+if(mouseY <= height/2) {
+	fill(221,56,56); //vermelho
+
+}
+else{
+	fill(19,52,146); //azul
+}
+
 	stroke(230);
 	strokeWeight(2);
-	noFill();
+	
 	circle(width / 2, height / 2, height / 4);
 
 
