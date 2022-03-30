c6721230d9acdef8fbcaa7477246df970e2ab643
AnaLopes99 <102222313+AnaLopes99@users.noreply.github.com>
2022-03-30 10:24:04 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,33 +8,34 @@ function setup() {
 
 function draw() {
 
-background(0)
+
 
 
 if(mouseY >= height/2) {
-noFill();
+	noFill();
 }
 
 else{
 
-fill(12,108,57); //verde
+	fill(12,108,57); //verde
 
 
 
 }
 
-if(mouseY <= height/2) {
-	fill(221,56,56); //vermelho
 
-}
-else{
+if(mouseX <= width/2) {
 	fill(19,52,146); //azul
 }
 
+if(mouseY < height/2 && mouseX <= width/2) {
+	fill(221,56,56); //vermelho
+
+}
 	stroke(230);
 	strokeWeight(2);
 	
-	circle(width / 2, height / 2, height / 4);
+	circle(width / 2, height / 2, height / 4); // circulo sempre ao centro do encrã
 
 
 }
